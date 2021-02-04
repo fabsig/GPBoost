53eac517683cc8abf135d2dda2308d82967ee0b2
fabsig <32902180+fabsig@users.noreply.github.com>
2021-02-04 16:32:44 +0100

    small changes

--- helpers/run_tests_coverage_R_package.R
+++ helpers/run_tests_coverage_R_package.R
@@ -1,5 +1,11 @@
 # Set working directory to GPBoost main folder
 
+# Run unit tests locally
+library(testthat)
+library(gpboost)
+path_tests = paste0(getwd(),.Platform$file.sep,file.path("R-package","tests","testthat"))
+test_dir(path_tests, reporter = "summary")
+
 # Evaluate coverage of R tests
 system("Rscript build_r.R")
 library(covr)
@@ -7,8 +13,3 @@ coverage  <- covr::package_coverage('./gpboost_r', quiet=FALSE)
 print(coverage)
 covr::report(coverage, browse = TRUE)
 
-# Run tests locally
-library(testthat)
-library(gpboost)
-path_tests = paste0(getwd(),.Platform$file.sep,file.path("R-package","tests","testthat"))
-test_dir(path_tests, reporter = "summary")
