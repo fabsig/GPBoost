d1d3f4d57cbc2290f856f73f40305d9790b3babf
fabsig <32902180+fabsig@users.noreply.github.com>
2021-03-12 13:32:18 +0100

    small change

--- helpers/check_R_package_rhub.R
+++ helpers/check_R_package_rhub.R
@@ -1,12 +1,12 @@
 # install.packages("rhub")
 library("rhub")
-# validate_email()
+validate_email()
 
 # First run 'sh build-cran-package.sh'
 # Set working directory to gpboost_r folder
 
-mycheck <- check()
+mycheck <- check(env_vars=c(R_COMPILE_AND_INSTALL_PACKAGES = "always"))
 mycheck$cran_summary()
 
-cran_prep <- check_for_cran()
+cran_prep <- check_for_cran(env_vars=c(R_COMPILE_AND_INSTALL_PACKAGES = "always"))
 cran_prep$cran_summary()
