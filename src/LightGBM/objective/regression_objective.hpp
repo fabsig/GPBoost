74ea80f09e3e746a1217daf6d88a659f48c10b2f
fabsig <32902180+fabsig@users.noreply.github.com>
2021-07-02 07:44:56 +0200

    small change

--- src/LightGBM/objective/regression_objective.hpp
+++ src/LightGBM/objective/regression_objective.hpp
@@ -281,31 +281,6 @@ namespace LightGBM {
 			return 0.5 * std::erfc(-value * M_SQRT1_2);
 		}
 
-		//For use below in normalCDFInverse
-		double RationalApproximation(double t) const {
-			// Abramowitz and Stegun formula 26.2.23.
-			// The absolute value of the error should be less than 4.5 e-4.
-			double c[] = { 2.515517, 0.802853, 0.010328 };
-			double d[] = { 1.432788, 0.189269, 0.001308 };
-			return t - ((c[2] * t + c[1]) * t + c[0]) /
-				(((d[2] * t + d[1]) * t + d[0]) * t + 1.0);
-		}
-
-		double normalCDFInverse(double p) const {
-			if (p <= 0.0 || p >= 1.0) {
-				Log::Fatal("Invalid input argument (%g); must be larger than 0 but less than 1.");
-			}
-
-			if (p < 0.5) {
-				// F^-1(p) = - G^-1(p)
-				return -RationalApproximation(sqrt(-2.0 * log(p)));
-			}
-			else {
-				// F^-1(p) = G^-1(1-p)
-				return RationalApproximation(sqrt(-2.0 * log(1 - p)));
-			}
-		}
-
 	protected:
 		bool sqrt_;
 		/*! \brief Number of data */
