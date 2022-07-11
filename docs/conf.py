3df8a5e76614683165c526386b4f7959c892a726
fabsig <32902180+fabsig@users.noreply.github.com>
2022-07-11 14:53:36 +0200

    [docs] set language = 'en' in Sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -140,7 +140,7 @@ with open(os.path.join(CURR_PATH, os.path.pardir, 'VERSION.txt'), 'r') as f:
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
