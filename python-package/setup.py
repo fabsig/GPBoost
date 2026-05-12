6d00df334a6ca53b49a661c2c4ed5061f4d3c94d
unknown <fabio.sigrist.hslu@gmail.com>
2026-05-12 07:38:18 +0200

    [python-package] add Python 3.14 classifier

--- python-package/setup.py
+++ python-package/setup.py
@@ -409,4 +409,5 @@ if __name__ == "__main__":
                        'Programming Language :: Python :: 3.11',
                        'Programming Language :: Python :: 3.12',
                        'Programming Language :: Python :: 3.13',
+                       'Programming Language :: Python :: 3.14',
                        'Topic :: Scientific/Engineering :: Artificial Intelligence'])
